1b325bd422a448efed8ebd673425816cf4209af7
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-02-17 02:28:39 +0600

    first commited, home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    colors: {
+      dark: "#041F31",
+      Primary: '#BDEBEA', 
+      lightPrimary: "#00F5A0",
+      lightSecondary: "#00D9F5",
+    },
   },
   plugins: [require("daisyui")],
-}
+};
